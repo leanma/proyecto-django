dac325353335afb496ea2b2249af11da70f3b7da
Leandro <leanma0011@gmail.com>
2022-10-03 19:18:30 -0300

    correcciones

--- proyectoClase/views.py
+++ proyectoClase/views.py
@@ -51,17 +51,14 @@ def crear_persona(request,nombre,apellido):
     
     persona=Persona(nombre=nombre,apellido=apellido,edad=random.randrange(1,99),fecha_nacimiento=datetime.now)
     persona.save()
-    
     template = loader.get_template('crear_persona.html')
-    template_renderizado=template.render({'personas':persona})
-    
+    template_renderizado=template.render({'persona':persona})
     
     return HttpResponse(template_renderizado)
 
 def ver_personas(request):
     
     personas=Persona.objects.all()
-    
     template = loader.get_template('ver_persona.html')
     template_renderizado=template.render({'personas':personas})
     
